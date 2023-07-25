2a0c7ce512cad9b91339fa951ddf65ddf12becf1
yeGenieee <cyj95428@gmail.com>
2023-07-25 22:39:57 +0900

    feat:#16: LoginCheckIntercpetor 생성 및 LoginCheckInterceptor가 작동되도록 추가

---
+++ app/src/main/java/com/picketing/www/application/config/WebConfig.java
@@ -0,0 +1,15 @@
+package com.picketing.www.application.config;
+
+import com.picketing.www.application.interceptor.LoginCheckInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoginCheckInterceptor());
+    }
+}
---
+++ app/src/main/java/com/picketing/www/application/interceptor/LoginCheckInterceptor.java
@@ -0,0 +1,23 @@
+package com.picketing.www.application.interceptor;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+@Slf4j
+public class LoginCheckInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+    }
+}
