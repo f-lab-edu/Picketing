4e4b9dfb48bbc28ad03e05acf844d7b77010c5d8
pollra <pollra32@naver.com>
2023-07-31 23:40:44 +0900

    feat:#14: api 테스트 작성

---
+++ app/src/test/java/com/picketing/www/presentation/controller/user/UserControllerTest.java
@@ -0,0 +1,28 @@
+package com.picketing.www.presentation.controller.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class UserControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    void test01() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders
+                        .get("/api/users/1")
+                        .accept(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(status().isOk());
+    }
+}
