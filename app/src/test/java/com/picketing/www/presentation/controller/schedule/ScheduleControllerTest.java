24be9fe10cd33bb784ff9dbceca737c02b16fdef
pollra <pollra32@naver.com>
2023-08-30 22:19:07 +0900

    테스트 코드 완료

--- app/src/test/java/com/picketing/www/presentation/controller/schedule/ScheduleControllerTest.java
+++ app/src/test/java/com/picketing/www/presentation/controller/schedule/ScheduleControllerTest.java
@@ -8,7 +8,6 @@ import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -38,29 +37,6 @@ class ScheduleControllerTest {
 	@Autowired
 	ScheduleRepository scheduleRepository;
 
-	@BeforeEach
-	void setUp() throws Exception {
-		List<DateScheduleView> testSchedule = new ArrayList<>();
-		testSchedule.add(new DateScheduleView(
-			new DateSchedulePersist(
-				1L, 1L, "One day show", LocalDate.of(2023, 01, 01), LocalDate.of(2023, 01, 01)
-			),
-			List.of(
-				new TimeSchedulePersist(1L, 1L, LocalTime.of(05, 00), LocalTime.of(07, 30))
-			)
-		));
-		testSchedule.add(new DateScheduleView(
-			new DateSchedulePersist(
-				2L, 1L, "One day show", LocalDate.of(2023, 01, 02), LocalDate.of(2023, 01, 02)
-			),
-			List.of(
-				new TimeSchedulePersist(2L, 2L, LocalTime.of(05, 00), LocalTime.of(07, 30))
-			)
-		));
-		scheduleRepository.save(testSchedule);
-
-	}
-
 	@Nested
 	@DisplayName("Schedule 조회")
 	class GetSchedules {
@@ -87,8 +63,6 @@ class ScheduleControllerTest {
 			));
 			scheduleRepository.save(testSchedule);
 
-			System.out.println("=============== 데이터 입력 종료 ===================");
-
 			MockHttpSession session = new MockHttpSession();
 			session.setAttribute("login_user", "test@email.com");
 			mockMvc.perform(
@@ -98,7 +72,9 @@ class ScheduleControllerTest {
 						.accept(MediaType.APPLICATION_JSON)
 						.queryParam("yearMonth", "2023-01")
 				).andDo(print())
-				.andExpect(status().isOk());
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$.schedules.length()").value(2))
+				.andExpect(jsonPath("$.schedules[0].timeSchedules.length()").value(1));
 		}
 	}
 }
