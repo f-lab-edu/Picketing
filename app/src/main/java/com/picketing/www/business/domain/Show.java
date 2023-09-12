1318c1b9aa283dd40508906bb2b9df8c6c86556d
pollra <pollra32@naver.com>
2023-09-12 23:57:53 +0900

    [Feature] 도메인 객체에 JPA 엔티티 적용: Show

--- app/src/main/java/com/picketing/www/business/domain/Show.java
+++ app/src/main/java/com/picketing/www/business/domain/Show.java
@@ -5,28 +5,52 @@ import java.time.LocalDate;
 import com.picketing.www.business.type.AgeGroup;
 import com.picketing.www.business.type.Genre;
 import com.picketing.www.business.type.SubGenre;
+import com.picketing.www.persistence.table.BaseEntity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
-@Builder
-public class Show {
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "SHOW")
+@Getter(AccessLevel.PROTECTED)
+@Builder(access = AccessLevel.PROTECTED)
+public class Show extends BaseEntity {
 
-	final Long id;
-	final String title;
-	final Genre genre;
-	final SubGenre subGenre;
-	final LocalDate startDate;
-	final LocalDate endDate;
-	final String venue;
-	final Long runningTime;
-	final Long intermission;
-	final AgeGroup ageGroup;
-	final String details;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
-	boolean isBookable;
+	private String title;
 
-	final Long ownerId;
+	@Enumerated(EnumType.STRING)
+	private Genre genre;
 
+	@Enumerated(EnumType.STRING)
+	private SubGenre subGenre;
+	private LocalDate startDate;
+	private LocalDate endDate;
+	private String venue;
+	private Long runningTime;
+	private Long intermission;
+
+	@Enumerated(EnumType.STRING)
+	private AgeGroup ageGroup;
+	private String details;
+	private boolean isBookable;
+	private Long ownerId;
+	
 	public void changeBookable(boolean flag) {
 		this.isBookable = flag;
 	}
