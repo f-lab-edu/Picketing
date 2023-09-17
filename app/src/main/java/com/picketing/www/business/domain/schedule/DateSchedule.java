7c4d2f3bd0e7531890b8c751b7f6ebffb7379d63
yeGenieee <cyj95428@gmail.com>
2023-09-18 00:02:38 +0900

    DateSchedule & TimeSchedule 엔티티에 JPA 엔티티 적용 및 연관관계 매핑 적용

--- app/src/main/java/com/picketing/www/business/domain/schedule/DateSchedule.java
+++ app/src/main/java/com/picketing/www/business/domain/schedule/DateSchedule.java
@@ -1,15 +1,20 @@
 package com.picketing.www.business.domain.schedule;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 import com.picketing.www.business.domain.show.Show;
 import com.picketing.www.persistence.table.BaseEntity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -26,15 +31,19 @@ import lombok.NoArgsConstructor;
 public class DateSchedule extends BaseEntity {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "DATE_SCHEDULE_ID")
 	private Long id;
 
-	// TODO 연관관계 매핑
+	@ManyToOne
+	@JoinColumn(name = "SHOW_ID")
 	private Show show;
 
 	private String name;
+
 	private LocalDate startDate;
+
 	private LocalDate endDate;
 
-	// TODO 연관관계 매핑
-	List<TimeSchedule> timeSchedules;
+	@OneToMany(mappedBy = "dateSchedule")
+	private List<TimeSchedule> timeSchedules = new ArrayList<>();
 }
--- app/src/main/java/com/picketing/www/business/domain/schedule/TimeSchedule.java
+++ app/src/main/java/com/picketing/www/business/domain/schedule/TimeSchedule.java
@@ -2,21 +2,38 @@ package com.picketing.www.business.domain.schedule;
 
 import java.time.LocalTime;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "TIME_SCHEDULE")
+@Getter(AccessLevel.PROTECTED)
+@Builder(access = AccessLevel.PROTECTED)
 public class TimeSchedule {
-	final Long id;
-	final Long dateScheduleId;
-	final String name;
-	final LocalTime startTime;
-	final LocalTime endTime;
 
-	@Builder
-	public TimeSchedule(Long id, Long dateScheduleId, String name, LocalTime startTime, LocalTime endTime) {
-		this.id = id;
-		this.dateScheduleId = dateScheduleId;
-		this.name = name;
-		this.startTime = startTime;
-		this.endTime = endTime;
-	}
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "TIME_SCHEDULE_ID")
+	private Long id;
+
+	@ManyToOne
+	@JoinColumn(name = "DATE_SCHEDULE_ID")
+	private DateSchedule dateSchedule;
+	private String name;
+	private LocalTime startTime;
+	private LocalTime endTime;
+
 }
