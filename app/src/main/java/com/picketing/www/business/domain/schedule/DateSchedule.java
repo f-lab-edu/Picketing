93c65596dad70d637068b330f1b208bd6690479c
yeGenieee <cyj95428@gmail.com>
2023-09-17 13:55:10 +0900

    DateSchedule 도메인 객체에 JPA 적용

--- app/src/main/java/com/picketing/www/business/domain/schedule/DateSchedule.java
+++ app/src/main/java/com/picketing/www/business/domain/schedule/DateSchedule.java
@@ -3,19 +3,38 @@ package com.picketing.www.business.domain.schedule;
 import java.time.LocalDate;
 import java.util.List;
 
+import com.picketing.www.business.domain.show.Show;
+import com.picketing.www.persistence.table.BaseEntity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.Setter;
+import lombok.NoArgsConstructor;
 
-@Builder
-public class DateSchedule {
-	@Getter
-	final Long id;
-	final Long showId;
-	final String name;
-	final LocalDate startDate;
-	final LocalDate endDate;
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "DATE_SCHEDULE")
+@Getter(AccessLevel.PROTECTED)
+@Builder(access = AccessLevel.PROTECTED)
+public class DateSchedule extends BaseEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
-	@Setter
+	// TODO 연관관계 매핑
+	private Show show;
+
+	private String name;
+	private LocalDate startDate;
+	private LocalDate endDate;
+
+	// TODO 연관관계 매핑
 	List<TimeSchedule> timeSchedules;
 }
