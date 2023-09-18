d0893dd1b5927034778c61ebca38bb176dd49dce
yeGenieee <cyj95428@gmail.com>
2023-09-18 20:23:09 +0900

    Response key snake_case로 변경

--- app/src/main/java/com/picketing/www/presentation/dto/response/show/ShowSeatPriceResponse.java
+++ app/src/main/java/com/picketing/www/presentation/dto/response/show/ShowSeatPriceResponse.java
@@ -3,15 +3,22 @@ package com.picketing.www.presentation.dto.response.show;
 import java.math.BigDecimal;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.Builder;
 
 @Builder
 public record ShowSeatPriceResponse(
+
+	@JsonProperty("basic_price_list")
 	List<ShowSeatBasicPriceResponseDto> basicPriceList
 ) {
 
 	public record ShowSeatBasicPriceResponseDto(
+		@JsonProperty("show_id")
 		Long showId,
+
+		@JsonProperty("seat_grade_name")
 		String seatGradeName,
 		BigDecimal price
 	) {
