97fdb0456f9fa131c38a4897815f2ae19afe1c87
yeGenieee <cyj95428@gmail.com>
2023-09-25 20:54:53 +0900

    지연 로딩 추가

--- app/src/main/java/com/picketing/www/business/domain/schedule/TimeSchedule.java
+++ app/src/main/java/com/picketing/www/business/domain/schedule/TimeSchedule.java
@@ -4,6 +4,7 @@ import java.time.LocalTime;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -29,9 +30,10 @@ public class TimeSchedule {
 	@Column(name = "TIME_SCHEDULE_ID")
 	private Long id;
 
-	@ManyToOne
+	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "DATE_SCHEDULE_ID")
 	private DateSchedule dateSchedule;
+	
 	private String name;
 	private LocalTime startTime;
 	private LocalTime endTime;
