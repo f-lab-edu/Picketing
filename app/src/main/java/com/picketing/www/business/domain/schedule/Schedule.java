70af0a18dec0c5278fc3b234e3f741b366104230
yeGenieee <cyj95428@gmail.com>
2023-10-01 23:58:24 +0900

    공연장이 하나만 있는 것으로 가정하므로, 현재 상황에서는 공연장 엔티티 제외

--- app/src/main/java/com/picketing/www/business/domain/schedule/Schedule.java
+++ app/src/main/java/com/picketing/www/business/domain/schedule/Schedule.java
@@ -2,9 +2,17 @@ package com.picketing.www.business.domain.schedule;
 
 import java.util.List;
 
+import com.picketing.www.business.domain.show.seat.SeatGrade;
+
 import lombok.Builder;
 
 public class Schedule {
+
+	private Long showId;
+
+	// TODO 매핑 테이블로 빼야함
+	private List<SeatGrade> seatGradeList; // 공연장 정보
+
 	List<DateSchedule> dateSchedules;
 
 	@Builder
