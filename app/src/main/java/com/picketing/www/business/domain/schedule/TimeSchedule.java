6bc5dadc24d44a384a87373108d9b8fd75fd483c
yeGenieee <cyj95428@gmail.com>
2023-10-01 21:06:16 +0900

    Schedule 엔티티 다시 작성

--- app/src/main/java/com/picketing/www/business/domain/schedule/TimeSchedule.java
+++ app/src/main/java/com/picketing/www/business/domain/schedule/TimeSchedule.java
@@ -2,21 +2,33 @@ package com.picketing.www.business.domain.schedule;
 
 import java.time.LocalTime;
 
+import com.picketing.www.persistence.table.BaseEntity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.NoArgsConstructor;
 
-public class TimeSchedule {
-	final Long id;
-	final Long dateScheduleId;
-	final String name;
-	final LocalTime startTime;
-	final LocalTime endTime;
+@Entity
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder(access = AccessLevel.PROTECTED)
+@Table(name = "TIME_SCHEDULE")
+public class TimeSchedule extends BaseEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
-	@Builder
-	public TimeSchedule(Long id, Long dateScheduleId, String name, LocalTime startTime, LocalTime endTime) {
-		this.id = id;
-		this.dateScheduleId = dateScheduleId;
-		this.name = name;
-		this.startTime = startTime;
-		this.endTime = endTime;
-	}
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "date_schedule_id")
+	private DateSchedule DateSchedule;
+	private LocalTime startTime;
 }
