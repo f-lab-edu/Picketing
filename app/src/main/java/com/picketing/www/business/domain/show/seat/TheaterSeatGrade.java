3a9b2d5bf7f1b53dd7cb4d92bd5627da040756cd
yeGenieee <cyj95428@gmail.com>
2023-10-02 20:07:21 +0900

    DDL문 수정 (SHOW -> SHOWS, USER -> USERS로 테이블명 변경)

--- app/src/main/java/com/picketing/www/business/domain/show/seat/TheaterSeatGrade.java
+++ app/src/main/java/com/picketing/www/business/domain/show/seat/TheaterSeatGrade.java
@@ -1,16 +1,12 @@
 package com.picketing.www.business.domain.show.seat;
 
-import com.picketing.www.business.domain.schedule.Schedule;
 import com.picketing.www.persistence.table.BaseEntity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -29,10 +25,10 @@ public class TheaterSeatGrade extends BaseEntity {
 	@Id
 	@GeneratedValue
 	private Long id;
-
-	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "schedule_id")
-	private Schedule theaterSchedule;
+	//
+	// @ManyToOne(fetch = FetchType.LAZY)
+	// @JoinColumn(name = "schedule_id")
+	// private Schedule theaterSchedule;
 
 	@Enumerated(EnumType.STRING)
 	private SeatGrade seatGrade;
--- app/src/main/resources/db.migration/V1.0__init_ddl.sql
+++ app/src/main/resources/db.migration/V1.0__init_ddl.sql
@@ -1,4 +1,4 @@
-DROP TABLE IF EXISTS `SHOW`;
+DROP TABLE IF EXISTS `SHOWS`;
 DROP TABLE IF EXISTS `SCHEDULE`;
 DROP TABLE IF EXISTS `DATE_SCHEDULE`;
 DROP TABLE IF EXISTS `TIME_SCHEDULE`;
@@ -8,7 +8,7 @@ DROP TABLE IF EXISTS `TIME_SCHEDULE_SEAT_GRADE`;
 DROP TABLE IF EXISTS `RESERVATION`;
 DROP TABLE IF EXISTS `RESERVED_SEAT`;
 
-CREATE TABLE `SHOW`
+CREATE TABLE `SHOWS`
 (
     `id`           BIGINT       NOT NULL,
     `title`        VARCHAR(255) NOT NULL COMMENT '공연 제목',
@@ -24,8 +24,8 @@ CREATE TABLE `SHOW`
     `is_bookable`  TINYINT      NOT NULL DEFAULT 0 COMMENT '공연 예매 가능 여부',
     `owner_id`     BIGINT       NOT NULL COMMENT '공연 주최자 id',
     `created_at`   DATETIME     NOT NULL DEFAULT NOW(),
-    `modified_at`  DATETIME NULL DEFAULT NULL,
-    index          idx_show_title (`title`)
+    `modified_at`  DATETIME     NULL     DEFAULT NULL,
+    index idx_show_title (`title`)
 ) engine = InnoDB
   DEFAULT CHARSET = utf8mb4;
 
@@ -34,7 +34,7 @@ CREATE TABLE `SCHEDULE`
     `id`          BIGINT       NOT NULL,
     `show_id`     BIGINT       NOT NULL,
     `created_at`  VARCHAR(255) NOT NULL DEFAULT NOW(),
-    `modified_at` DATETIME NULL DEFAULT NULL
+    `modified_at` DATETIME     NULL     DEFAULT NULL
 ) engine = InnoDB
   DEFAULT CHARSET = utf8mb4;
 
@@ -44,7 +44,7 @@ CREATE TABLE `DATE_SCHEDULE`
     `schedule_id` BIGINT   NOT NULL,
     `show_date`   DATE     NOT NULL COMMENT '공연 일자',
     `created_at`  DATETIME NOT NULL DEFAULT NOW(),
-    `modified_at` DATETIME NULL DEFAULT NULL
+    `modified_at` DATETIME NULL     DEFAULT NULL
 ) engine = InnoDB
   DEFAULT CHARSET = utf8mb4;
 
@@ -54,7 +54,7 @@ CREATE TABLE `TIME_SCHEDULE`
     `date_schedule_id` BIGINT   NOT NULL,
     `start_time`       DATETIME NOT NULL COMMENT '공연 시작 시각',
     `created_at`       DATETIME NOT NULL DEFAULT NOW(),
-    `modified_at`      DATETIME NULL DEFAULT NULL
+    `modified_at`      DATETIME NULL     DEFAULT NULL
 ) engine = InnoDB
   DEFAULT CHARSET = utf8mb4;
 
@@ -64,10 +64,10 @@ CREATE TABLE `USERS`
     `email`        VARCHAR(255) NOT NULL COMMENT '사용자 이메일 주소',
     `password`     VARCHAR(255) NOT NULL COMMENT '사용자 계정 비밀번호',
     `name`         VARCHAR(50)  NOT NULL COMMENT '사용자 이름',
-    `phone_number` VARCHAR(30) NULL COMMENT '사용자 연락처',
+    `phone_number` VARCHAR(30)  NULL COMMENT '사용자 연락처',
     `created_at`   DATETIME     NOT NULL DEFAULT NOW(),
-    `modified_at`  DATETIME NULL DEFAULT NULL,
-    index          idx_user_email (`email`)
+    `modified_at`  DATETIME     NULL     DEFAULT NULL,
+    index idx_user_email (`email`)
 ) engine = InnoDB
   DEFAULT CHARSET = utf8mb4;
 
@@ -77,7 +77,7 @@ CREATE TABLE `THEATER_SEAT_GRADE`
     `schedule_id` BIGINT       NOT NULL,
     `seat_grade`  VARCHAR(255) NOT NULL COMMENT '공연장의 좌석 등급',
     `created_at`  DATETIME     NOT NULL DEFAULT NOW(),
-    `modified_at` DATETIME NULL DEFAULT NULL
+    `modified_at` DATETIME     NULL     DEFAULT NULL
 ) engine = InnoDB
   DEFAULT CHARSET = utf8mb4;
 
@@ -87,18 +87,18 @@ CREATE TABLE `TIME_SCHEDULE_SEAT_GRADE`
     `time_schedule_id` BIGINT       NOT NULL,
     `seat_grade`       VARCHAR(255) NOT NULL COMMENT '특정 시간대의 좌석 등급',
     `created_at`       DATETIME     NOT NULL DEFAULT NOW(),
-    `modified_at`      DATETIME NULL DEFAULT NULL
+    `modified_at`      DATETIME     NULL     DEFAULT NULL
 ) engine = InnoDB
   DEFAULT CHARSET = utf8mb4;
 
 CREATE TABLE `RESERVATION`
 (
-    `id`          BIGINT   NOT NULL,
-    `user_id`     BIGINT   NOT NULL,
-    `show_id`     BIGINT   NOT NULL,
+    `id`          BIGINT       NOT NULL,
+    `user_id`     BIGINT       NOT NULL,
+    `show_id`     BIGINT       NOT NULL,
     `Field3`      VARCHAR(255) NULL,
-    `created_at`  DATETIME NOT NULL DEFAULT NOW(),
-    `modified_at` DATETIME NOT NULL DEFAULT NULL
+    `created_at`  DATETIME     NOT NULL DEFAULT NOW(),
+    `modified_at` DATETIME     NOT NULL DEFAULT NULL
 ) engine = InnoDB
   DEFAULT CHARSET = utf8mb4;
 
@@ -108,11 +108,11 @@ CREATE TABLE `RESERVED_SEAT`
     `reservation_id`              BIGINT   NOT NULL,
     `time_schedule_seat_grade_id` BIGINT   NOT NULL,
     `created_at`                  DATETIME NOT NULL DEFAULT NOW(),
-    `modified_at`                 DATETIME NULL DEFAULT NULL
+    `modified_at`                 DATETIME NULL     DEFAULT NULL
 ) engine = InnoDB
   DEFAULT CHARSET = utf8mb4;
 
-ALTER TABLE `SHOW`
+ALTER TABLE `SHOWS`
     ADD CONSTRAINT `PK_SHOW` PRIMARY KEY (
                                           `id`
         );
@@ -132,7 +132,7 @@ ALTER TABLE `TIME_SCHEDULE`
                                                    `id`
         );
 
-ALTER TABLE `USER`
+ALTER TABLE `USERS`
     ADD CONSTRAINT `PK_USER` PRIMARY KEY (
                                           `id`
         );
@@ -161,8 +161,8 @@ ALTER TABLE `SCHEDULE`
     ADD CONSTRAINT `FK_SHOW_TO_SCHEDULE_1` FOREIGN KEY (
                                                         `show_id`
         )
-        REFERENCES `SHOW` (
-                           `id`
+        REFERENCES `SHOWS` (
+                            `id`
             );
 
 ALTER TABLE `DATE_SCHEDULE`
@@ -201,16 +201,16 @@ ALTER TABLE `RESERVATION`
     ADD CONSTRAINT `FK_USER_TO_RESERVATION_1` FOREIGN KEY (
                                                            `user_id`
         )
-        REFERENCES `USER` (
-                           `id`
+        REFERENCES `USERS` (
+                            `id`
             );
 
 ALTER TABLE `RESERVATION`
     ADD CONSTRAINT `FK_SHOW_TO_RESERVATION_1` FOREIGN KEY (
                                                            `show_id`
         )
-        REFERENCES `SHOW` (
-                           `id`
+        REFERENCES `SHOWS` (
+                            `id`
             );
 
 ALTER TABLE `RESERVED_SEAT`
