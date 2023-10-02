6a86ba45d0413ee0bf751d9d4f52608ec1ce7478
yeGenieee <cyj95428@gmail.com>
2023-10-02 10:17:11 +0900

    SeatGrade 엔티티 내 주석 추가

--- app/src/main/java/com/picketing/www/business/domain/show/seat/SeatGrade.java
+++ app/src/main/java/com/picketing/www/business/domain/show/seat/SeatGrade.java
@@ -15,7 +15,7 @@ public enum SeatGrade {
 
 	private final String name; // 공연장의 좌석 명칭
 
-	private final Long price; // 공연장의 좌석 가격
+	private final Long price; // 공연장의 좌석 가격 (원래는 공연마다 가격이 달라지므로, 이 엔티티 필드가 아니지만, 현재는 공연 좌석이 고정되어 있으므로 price를 공연 좌석에 둡니다)
 
 	private final int count; // 공연장의 좌석 개수
 }
