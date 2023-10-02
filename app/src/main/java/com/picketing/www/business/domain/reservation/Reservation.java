8c76c2e9e6939a54ebc029657b8c52477bfa1dad
yeGenieee <cyj95428@gmail.com>
2023-10-02 17:52:09 +0900

    예매 및 좌석 예매 도메인 추가

---
+++ app/src/main/java/com/picketing/www/business/domain/reservation/Reservation.java
@@ -0,0 +1,47 @@
+package com.picketing.www.business.domain.reservation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.data.annotation.Id;
+
+import com.picketing.www.business.domain.User;
+import com.picketing.www.business.domain.show.Show;
+import com.picketing.www.persistence.table.BaseEntity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "RESERVATION")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder(access = AccessLevel.PROTECTED)
+@Getter(AccessLevel.PROTECTED)
+public class Reservation extends BaseEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "show_id")
+	private Show show;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id")
+	private User user;
+
+	@OneToMany(mappedBy = "reservation")
+	private List<ReservedSeat> reservedSeatList = new ArrayList<>();
+}
