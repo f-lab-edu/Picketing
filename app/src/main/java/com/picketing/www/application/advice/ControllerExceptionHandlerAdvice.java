2e64edaa090aa5ba457da94ce185629a6e474d41
yeGenieee <cyj95428@gmail.com>
2023-10-23 11:25:11 +0900

    불필요한 콘솔 출력 제거

--- app/src/main/java/com/picketing/www/application/advice/ControllerExceptionHandlerAdvice.java
+++ app/src/main/java/com/picketing/www/application/advice/ControllerExceptionHandlerAdvice.java
@@ -14,7 +14,6 @@ public class ControllerExceptionHandlerAdvice {
 
 	@ExceptionHandler(Exception.class)
 	public ResponseEntity<ErrorResponse> otherExceptionHandler(Exception ex) {
-		System.out.println("ex.getMessage() = " + ex.getMessage());
 		return ResponseEntity
 			.status(HttpStatus.INTERNAL_SERVER_ERROR)
 			.body(ErrorResponse.error(ErrorCode.INTERNAL_UNKNOWN_ERROR));
@@ -22,7 +21,6 @@ public class ControllerExceptionHandlerAdvice {
 
 	@ExceptionHandler(CustomException.class)
 	public ResponseEntity<ErrorResponse> customExceptionHandler(CustomException ex) {
-		System.out.println("ex.getMessage() = " + ex.getMessage());
 		return ResponseEntity
 			.status(ex.getErrorCode().getHttpStatus())
 			.body(ErrorResponse.error(ex.getErrorCode()));
